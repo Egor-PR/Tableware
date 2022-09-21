fd788e5b9df4266de34acd706c0faae2bad36645
Egor <beltyugov94@mail.com>
2022-09-21 15:43:46 +0300

    commits

--- app/js/main.js
+++ app/js/main.js
@@ -22,80 +22,30 @@ const data = [
         price: 1600
     },
 ]
+let sum = 0; 
 
-const count = document.querySelectorAll('[content__block]')
-let sum = 0;
+const products = document.querySelectorAll('.content__block-product'); 
+const buttons = document.querySelectorAll('.content__block-button');
+const sumSelector = document.querySelector('.hero__sum'); 
+sumSelector.textContent = sum; 
 
-//  console.log(data[0].price)
-// const result = data.find(el => el.price ===2);
-// console.log('result',result);
-// const sum = document.querySelector('.hero__sum');
-// const sum = 0;
-// for (let i=0; i<data.length; i++){
-//     console.log(i)
-// }
+products.forEach((product, index) => {
+    const productTitle = product.querySelector('.content__block-title'); 
+    productTitle.textContent = data[index].name; 
+    const productPrice = product.querySelector('.content__block-price'); 
+    productPrice.textContent = data[index].price; 
+    const formattedPrice = numberWithSpaces(data[index].price);
+    console.log(formattedPrice)
+    productPrice.textContent = `${formattedPrice} РУБ.`; 
+});
 
-
-   // const key = data.find (el=> el.price===1200);
-
-    // var num1,result;
-    // num1=  getElementById('1').data;
-   
-    // num1 = parseInt(num1);
-    //  result = price+ price ;
-    // document.getElementById('sum').innerHTML = result
-
-//  num2=document.getElementById('2').data;
-//     num2 =parseInt(num2);
-//     num3=document.getElementById('3').data;
-//     num3 =parseInt(num3);
-//     result = num1+num2;
-//     document.getElementById('sum').innerHTML = result
-
-// let someData = data.filter(item=>item.id<4);
-// console.log(someData.length);
-
-
-// const count = document.querySelectorAll('[content__block]');
-// if (count){
-//     count.forEach(coun=>{
-//         count.addEventListener('click',e =>{
-//             const target = e.target;
-//             if(target.closest('.cont ent__block-button')){
-//                 let value = parseInt(target.closest('.count').querySelector('input').value);
-
-//                 if(target.classList.count('add')){
-//                     value++;
-//                 }
-//                 target.closest('.count').querySelector('input').value = value;
-//             }
-//         })
-//     })
-// }
-
-
-
- document.onclick = event => {
-    // console.log(event.target.classList);
-    if(event.target.classList.contains('add')){
-        plusFunction(event.target.dataset.id);
-    }
- };
- const plusFunction = id=>{
-    data[id]['price']++;
-    renderData();
-    
- }
- const renderData = ()=>{
-    console.log(data);
- }
- renderData();
- let j = 0;
-document.addEventListener('click', function (){
-    data[0]['price'] +data[1]['price']
-    console.log('click');
-}); 
-//  let  add = document.querySelector("btn");
-//  let sum = document.querySelector("sum");
-//  add.addEventListener('click', function(){
-// });
+buttons.forEach((button, index) => {
+    button.addEventListener('click', e => {
+        button.classList.add('.button_disabled');
+        sum += data[index].price; 
+        sumSelector.textContent = sum; 
+    });
+});
+function numberWithSpaces(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' '); 
+  }
